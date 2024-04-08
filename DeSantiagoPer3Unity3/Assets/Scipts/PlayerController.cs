7a7678b0cccf29aa6c64a2f9ba6cb61a52611cb0
Alfredo De Santiago <727601@student.dusd.net>
2024-04-08 11:37:21 -0700

    Started a project
    started a new project

---
+++ DeSantiagoPer3Unity3/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+       
+    }
+
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
